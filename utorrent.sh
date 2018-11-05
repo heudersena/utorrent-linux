0caae18ccd5a1191d2161615227c78f746e5c23b
Heuder Rodrigues de Sena <documento_arquivo@hotmail.com>
2018-11-05 13:07:36 -0200

    script install utorrent 64 bits
    script install utorrent 64 bits

---
+++ utorrent.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# País: Brasil
+# Author:  Heuder Rodrigues de Sena
+# E-mail: programadorwebti@gmail.com
+# Telegram: @Heuderrodriguesdesena
+# Phone: +5561993029884
+
+sudo apt-get upgrade
+sudo apt-get install libssl1.0.0 libssl-dev
+wget wget http://download.ap.bittorrent.com/track/beta/endpoint/utserver/os/linux-x64-ubuntu-13-04 -O utserver.tar.gz
+sudo tar -zxvf utserver.tar.gz -C /opt/
+sudo chmod 777 /opt/utorrent-server-alpha-v3_3/
+sudo ln -s /opt/utorrent-server-alpha-v3_3/utserver /usr/local/bin
+utserver -settingspath /opt/utorrent-server-alpha-v3_3/ &
+
+
+cd /etc/init.d
+touch utorrent.sh
+chmod 777 utorrent.sh
+echo "#!/bin/bash" >> utorrent.sh
+echo "utserver -settingspath /opt/utorrent-server-alpha-v3_3/ &" >> utorrent.sh
+update-rc.d utorrent.sh defaults
+chmod 777 utorrent.sh
